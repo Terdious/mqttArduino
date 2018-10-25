1807adefba8328725b3cfcf377991f2c6f5d0e5c
Terdious <35010958+Terdious@users.noreply.github.com>
2018-10-25 18:45:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ sails.log.info(`Arduino - Successfully connected to MQTT: OK12`);
     // Called when a user changes a device's state
     return {
 sails.log.info(`Arduino - Successfully connected to MQTT: OK13`);
-        exec();
+        exec
 sails.log.info(`Arduino - Successfully connected to MQTT: OK14`);
     };
 };
